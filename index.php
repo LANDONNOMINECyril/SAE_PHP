0270bc5b0931579b7edfd4c40315eec25ddeba8b
LANDON--NOMINE Cyril <o22202952@info27-08.iut45.univ-orleans.fr>
2024-02-01 15:14:28 +0100

    feature: debut connexion, queles problèmes rencontrés, suite au prochain commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    require_once 'Classes/Autoloader.php';
+    Autoloader::register();
+
+    require_once 'Classes/data/bd.php';
+    createBD();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Application spotify</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
