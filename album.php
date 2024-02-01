d2ba0b494997771a9f805cc5f25476a07ce031bb
Theo45120 <theo.villain45000@gmail.com>
2024-02-01 12:08:46 +0100

    page album et compte commencé

--- album.php
+++ album.php
@@ -3,24 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Votre Page</title>
-  <!-- Inclure Bootstrap CSS -->
+  <title>Page Album</title>
+  <!-- le bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
-<header>
-    <h3><input type="text" placeholder="Rechercher un album..."></h3>
-</header>
+
 
 <aside>
   <nav>
     <ul>
-      <li><a href="index.php">Explorer la liste d'album</a></li>
+      <li><a href="#">Explorer la liste d'album</a></li>
       <li><a href="#">Mes favoris</a></li>        
       <li><a href="#">Mon historique</a></li>
-      <li><a href="monCompte.php">Mon Compte</a></li>
+      <li><a href="#">Mon Compte</a></li>
       <li id="bot1" ><a href="#">Déconnexion</a></li>
       <li id="bot2" ><a href="#">Quitter l'application</a></li>
 
@@ -29,9 +27,7 @@
 </aside>
 
 <main>
-  <!-- Contenu principal de votre page -->
-  <h2>Contenu principal</h2>
-  <p>Bienvenue sur votre page. Vous pouvez ajouter votre contenu ici.</p>
+  
 </main>
 
 <!-- Inclure Bootstrap JS (jQuery et Popper.js doivent être inclus avant) -->
