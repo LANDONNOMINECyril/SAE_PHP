ae0a43db19416eb0e3b467e5c8e68de7e6d96a41
Theo45120 <theo.villain45000@gmail.com>
2024-02-15 17:01:33 +0100

    ajout des albums dans la page artistes

--- pageArtiste.php
+++ pageArtiste.php
@@ -7,6 +7,7 @@
   <!-- le bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="static/album.css">
 </head>
 <body>
 
@@ -37,8 +38,10 @@
     require "Classes/Autoloader.php";
     Autoloader::register();
     Autoloader::autoload("bd\ArtisteBD");
+    Autoloader::autoload("bd\AlbumBD");
     use Symfony\Component\Yaml\Yaml;
     $artiste = \Classes\bd\ArtisteBD::getById($_GET['artist_id']);
+    $albums = \Classes\bd\AlbumBD::getByArtiste($_GET['artist_id']);
 
     $debut = "fixtures/images/";
     $url = "";
@@ -60,7 +63,65 @@
     if ($admin) {
         echo "<a href='modifierArtiste.php?artiste_id=" . $artiste->getId() . "'>Modifier</a>";
     }
+
+    echo "<h2> Liste des albums de " . $artiste->getNom() . " :</h2>";
     ?>
+    <?php
+    if(!is_array($albums)){ ?>
+                <div class="artiste">
+                <img src="<?php echo $url; ?>" alt="artiste1"/>
+
+                <div class="contenu">
+                    <a href="album.php?album_id=<?php echo $album->getId(); ?>"><h3 class="test-arrow">
+                            <span><?php echo $album->getTitre(); ?></span></h3></a>
+
+                    <a href="pageArtiste.php?artist_id=<?php echo $album->getArtisteId(); ?>"><p
+                                class="test-arrow"><?php echo $album->getArtiste(); ?></p></a>
+                    <?php
+                    if ($admin) {
+                        echo "<a href='adminAlbum.php?type=modif&album_id=" . $album->getId() . "'><p class='test-arrow'>Modifier</p></a>";
+                        echo "<a onclick=\"showPopupDel('" . $album->getTitre() . "')\"><p class='test-arrow'>Supprimer</p></a>";
+                    }
+                    ?>
+                </div>
+            </div>
+            
+       <?php }
+        
+    foreach ($albums as $album) :
+            $debut = "fixtures/images/";
+            $url = "";
+            if ($album->getUrlImage() != "" && $album->getUrlImage() != "img.jpg") {
+                $url = $debut . $album->getUrlImage();
+
+            }
+
+            if ($album->getUrlImage() === "") {
+                $album->setUrlImage("default.jpg");
+                $url = $debut . $album->getUrlImage();
+            }
+
+            //print_r($album) // TESTTTT
+            //print_r($album->getArtiste())
+            ?>
+            <div class="artiste">
+                <img src="<?php echo $url; ?>" alt="artiste1"/>
+
+                <div class="contenu">
+                    <a href="album.php?album_id=<?php echo $album->getId(); ?>"><h3 class="test-arrow">
+                            <span><?php echo $album->getTitre(); ?></span></h3></a>
+
+                    <a href="pageArtiste.php?artist_id=<?php echo $album->getArtisteId(); ?>"><p
+                                class="test-arrow"><?php echo $album->getArtiste(); ?></p></a>
+                    <?php
+                    if ($admin) {
+                        echo "<a href='adminAlbum.php?type=modif&album_id=" . $album->getId() . "'><p class='test-arrow'>Modifier</p></a>";
+                        echo "<a onclick=\"showPopupDel('" . $album->getTitre() . "')\"><p class='test-arrow'>Supprimer</p></a>";
+                    }
+                    ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
 </main>
 
 <!-- Inclure Bootstrap JS (jQuery et Popper.js doivent être inclus avant) -->
